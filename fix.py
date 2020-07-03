6306cb04dce9c5df47e1b8c85b687050b9ff5f26
Donghwi Kim <dhkim09a@gmail.com>
2020-07-03 23:45:04 +0900

    Update

---
+++ fix.py
@@ -0,0 +1,40 @@
+import configparser
+import os
+
+from pyutil.subcommand import Subcommand
+import argparse
+import shlex
+
+from save import get_current_display_settings, get_config_name
+
+
+class Fix(Subcommand):
+    NAME = 'fix'
+
+    def on_parser_init(self, parser: argparse.ArgumentParser):
+        pass
+
+    def on_command(self, args):
+        cp = configparser.ConfigParser()
+
+        if os.path.isfile(args.config):
+            cp.read(args.config)
+        else:
+            print('error: could not load config from ' + str(args.config))
+            return
+
+        ds = get_current_display_settings(args.displayplacer)
+
+        name = get_config_name(ds)
+
+        if not cp.has_section(name):
+            print('error: could not load config for displays ' + str(name))
+            return
+
+        print('resetting displays ...', end='', flush=True)
+        args.displayplacer(list(cp[name].values()))
+        print('done')
+
+    def __init__(self, subparsers):
+        super(self.__class__, self).__init__(self.NAME, subparsers)
+
---
+++ save.py
@@ -0,0 +1,53 @@
+import configparser
+import os
+
+from pyutil.subcommand import Subcommand
+import argparse
+import shlex
+
+
+def get_current_display_settings(displayplacer):
+    ds = []
+    for confline in shlex.split(str(displayplacer.list()).splitlines()[-1:][0])[1:]:
+        confline: str
+        for id in filter(lambda e: e.startswith('id:'), confline.split()):
+            ds.append((id[len('id:'):], confline))
+
+    return sorted(ds)
+
+
+def get_config_name(display_settings):
+    ids, configs = zip(*display_settings)
+    return ':'.join(ids)
+
+
+class Save(Subcommand):
+    NAME = 'save'
+
+    def on_parser_init(self, parser: argparse.ArgumentParser):
+        pass
+
+    def on_command(self, args):
+        cp = configparser.ConfigParser()
+
+        if os.path.isfile(args.config):
+            cp.read(args.config)
+
+        ds = get_current_display_settings(args.displayplacer)
+
+        name = get_config_name(ds)
+
+        for id, config in sorted(ds):
+            if not cp.has_section(name):
+                cp[name] = {}
+            cp[name][id] = config
+
+        if not os.path.isdir(os.path.dirname(args.config)):
+            os.makedirs(os.path.dirname(args.config), exist_ok=True)
+
+        with open(args.config, 'w+') as f:
+            cp.write(f)
+
+    def __init__(self, subparsers):
+        super(self.__class__, self).__init__(self.NAME, subparsers)
+
